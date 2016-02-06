d10f4eaca8bb085de210d61b78e33989249dfa25
helsont <helsont@gmail.com>
2016-02-05 23:36:02 -0500

    Added bing maps support

--- app/js/controllers/example.js
+++ app/js/controllers/example.js
@@ -1,14 +1,20 @@
-function ExampleCtrl() {
+(function() {
+  angular
+    .module('fasteats.main', MainController)
+    .controller('MainController', MainController);
+  console.log('yo');
+  MainController.$inject = ['$scope'];
 
-  // ViewModel
-  const vm = this;
-
-  vm.title = 'AngularJS, Gulp, and Browserify! Written with keyboards and love!';
-  vm.number = 1234;
-
-}
-
-export default {
-  name: 'ExampleCtrl',
-  fn: ExampleCtrl
-};
+  function MainController($scope) {
+    console.log('man');
+    $scope.mapOptions = {
+      center: { latitude: 30.614919, longitude: -96.342316},
+      zoom: 6,
+      mapType: 'a',
+      options: {
+      	disablePanning: true,
+      	disableZooming:true
+      }
+    };
+  }
+})
--- app/js/main.js
+++ app/js/main.js
@@ -1,35 +1,12 @@
-import angular from 'angular';
+angular.module('fast_eats', [
+  'angularBingMaps'
+])
+.run(function() {
 
-// angular modules
-import constants from './constants';
-import onConfig  from './on_config';
-import onRun     from './on_run';
-import 'angular-ui-router';
-import './templates';
-import './filters';
-import './controllers';
-import './services';
-import './directives';
-
-// create and bootstrap application
-const requires = [
-  'ui.router',
-  'templates',
-  'app.filters',
-  'app.controllers',
-  'app.services',
-  'app.directives'
-];
-
-// mount on window for testing
-window.app = angular.module('app', requires);
-
-angular.module('app').constant('AppSettings', constants);
-
-angular.module('app').config(onConfig);
-
-angular.module('app').run(onRun);
-
-angular.bootstrap(document, ['app'], {
-  strictDi: true
+})
+.config(function(angularBingMapsProvider) {
+  angularBingMapsProvider.setDefaultMapOptions({
+      credentials: 'AsdaxrvLRHyT6eEvSGDY81S2y3_ifh10egwFAMzIOLtxv0ZlwkPE2yG7jMzXzVkp',
+      enableClickableLogo: false
+  });
 });
